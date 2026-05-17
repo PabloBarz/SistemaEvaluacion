703838fc29699bb94f30c16411a7d0735769c4df
Pablo Barzola <robertho090504@gmail.com>
2026-05-17 18:33:24 -0500

    Estructura JSON y renderizado dinamico de preguntas

--- index.js
+++ index.js
@@ -25,18 +25,18 @@ function buscarPersona(){
     }
 
     let personas = JSON.parse(localStorage.getItem("personas")) || [];
-
     let personaEncontrada = personas.find(persona => persona.id === id);
 
     // PERSONA NO EXISTE
     if(!personaEncontrada){
+
+        alert("No existe la persona")
 
         txtNombre.value = "";
         txtApellido.value = "";
 
         btnRegistrar.classList.remove("hidden");
         btnIniciar.classList.add("hidden");
-
         txtNombre.focus();
 
         return;
@@ -62,7 +62,6 @@ function guardarPersona(){
     }
 
     let personas = JSON.parse(localStorage.getItem("personas")) || [];
-
     let personaExiste = personas.find(persona => persona.id === id);
 
     if(personaExiste){
@@ -81,17 +80,84 @@ function guardarPersona(){
 
     localStorage.setItem("personas", JSON.stringify(personas));
 
-    alert("Persona registrada");
-
-    clearInputs();
+    alert("Usuario registrado");
 
     btnRegistrar.classList.add("hidden");
+    btnIniciar.classList.remove("hidden");
+
+}
+
+async function renderizarPreguntas(){
+
+    let response = await fetch("preguntas.json");
+    let data = await response.json();
+    let contenedor = document.getElementById("contenedorPreguntas");
+
+    contenedor.innerHTML = "";
+    data.preguntas.forEach((pregunta, index) => {
+
+        let opcionesHTML = "";
+        pregunta.opciones.forEach(opcion => {
+
+            opcionesHTML += `
+                <label class="opcion">
+
+                    <input 
+                        type="radio"
+                        name="pregunta${pregunta.id}"
+                        value="${opcion.letra}"
+                    >
+
+                    ${opcion.letra}) ${opcion.texto}
+
+                </label>
+            `;
+
+        });
+
+        contenedor.innerHTML += `
+            <div class="pregunta">
+
+                <h3>
+                    ${index + 1}. ${pregunta.pregunta}
+                </h3>
+
+                <div class="opciones">
+                    ${opcionesHTML}
+                </div>
+
+            </div>
+        `;
+
+    });
+
+    contenedor.innerHTML += `
+        <button type="button" id="btnCalificar">
+            Ver Resultado
+        </button>
+    `;
+}
+
+function iniciarEvaluacion(){
+
+    let id = txtId.value.trim();
+    let personas = JSON.parse(localStorage.getItem("personas")) || [];
+    let personaEncontrada = personas.find(persona => persona.id === id);
+
+    // INCREMENTAR INTENTOS
+    personaEncontrada.intentos++;
+
+    // GUARDAR CAMBIOS
+    localStorage.setItem("personas", JSON.stringify(personas));
+
+    renderizarPreguntas();
 }
 
 
 //EVENTOS
 btnBuscar.addEventListener("click", buscarPersona);
 btnRegistrar.addEventListener("click", guardarPersona);
+btnIniciar.addEventListener("click", iniciarEvaluacion);
 txtId.addEventListener("keypress", function(event){
 
     if(event.key === "Enter"){
