84b9b4543143518bf2a1359251184f49c10a9beb
Pablo Barzola <robertho090504@gmail.com>
2026-05-17 18:05:44 -0500

    Formulario de usuario y estilos base del panel

---
+++ index.js
@@ -0,0 +1,9 @@
+// INPUTS
+const txtId = document.getElementById("txtId");
+const txtNombre = document.getElementById("txtNombre");
+const txtApellido = document.getElementById("txtApellido");
+
+// BOTONES
+const btnBuscar = document.getElementById("btnBuscar");
+const btnRegistrar = document.getElementById("btnRegistrar");
+const btnIniciar = document.getElementById("btnIniciar");
