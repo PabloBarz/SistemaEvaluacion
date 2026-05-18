856ec1e27f0f6d7b76eab44b003c4971f8a91e45
Pablo Barzola <robertho090504@gmail.com>
2026-05-17 21:15:57 -0500

    Sistema de calificacion y validacion de respuestas, rediseño en css

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ function mezclarArray(array){
 }
 
 function buscarPersona(){
+    let contenedorPreguntas = document.getElementById("contenedorPreguntas")
+    let resultado = document.getElementById("resultadoEvaluacion")
 
     let id = txtId.value.trim();
 
     if(!id){
         alert("Ingrese un ID");
+        contenedorPreguntas.innerHTML = ""
+        resultado.innerHTML = ""
         txtId.focus();
         return;
     }
@@ -36,6 +40,8 @@ function buscarPersona(){
 
         alert("No existe la persona")
 
+        contenedorPreguntas.innerHTML = ""
+        resultado.innerHTML = ""
         txtNombre.value = "";
         txtApellido.value = "";
 
@@ -49,6 +55,8 @@ function buscarPersona(){
     // PERSONA EXISTE
     txtNombre.value = personaEncontrada.nombre;
     txtApellido.value = personaEncontrada.apellido;
+    contenedorPreguntas.innerHTML = ""
+    resultado.innerHTML = ""
 
     btnIniciar.classList.remove("hidden");
     btnRegistrar.classList.add("hidden");
@@ -59,6 +67,7 @@ function guardarPersona(){
     let id = txtId.value.trim();
     let nombre = txtNombre.value.trim();
     let apellido = txtApellido.value.trim();
+
 
     if(!id || !nombre || !apellido){
         alert("Complete todos los campos");
@@ -97,6 +106,9 @@ async function renderizarPreguntas(){
     let data = await response.json();
     let contenedor = document.getElementById("contenedorPreguntas");
 
+    let resultado = document.getElementById("resultadoEvaluacion");
+    resultado.classList.add("hidden");
+
     contenedor.innerHTML = "";
     let preguntasMezcladas = mezclarArray([...data.preguntas]);
     preguntasMezcladas.forEach((pregunta, index) => {
@@ -111,7 +123,7 @@ async function renderizarPreguntas(){
                     <input 
                         type="radio"
                         name="pregunta${pregunta.id}"
-                        value="${value="${opcion.correcta}"}"
+                        value="${opcion.correcta}"
                     >
 
                     ${String.fromCharCode(65 + opcionIndex)}) ${opcion.texto}
@@ -139,9 +151,91 @@ async function renderizarPreguntas(){
 
     contenedor.innerHTML += `
         <button type="button" id="btnCalificar">
-            Ver Resultado
+            Finalizar Evaluación
         </button>
     `;
+
+    let btnCalificar = document.getElementById("btnCalificar");
+    btnCalificar.addEventListener("click", calificarExamen);
+}
+
+async function calificarExamen(){
+
+    let response = await fetch("preguntas.json");
+    let data = await response.json();
+    let correctas = 0;
+
+    data.preguntas.forEach(pregunta => {
+
+        let respuestaSeleccionada = document.querySelector(
+            `input[name="pregunta${pregunta.id}"]:checked`
+        );
+
+        // NO RESPONDIO
+        if(!respuestaSeleccionada){
+            return;
+        }
+
+        // RESPUESTA CORRECTA
+        if(respuestaSeleccionada.value === "true"){
+            correctas++;
+        }
+
+    });
+
+    let totalPreguntas = data.preguntas.length;
+    let incorrectas = totalPreguntas - correctas;
+    let nota = (correctas / totalPreguntas) * 20;
+
+    mostrarResultado(correctas, incorrectas, nota);
+}
+
+
+function mostrarResultado(correctas, incorrectas, nota){
+
+    let contenedorPreguntas = document.getElementById("contenedorPreguntas");
+    contenedorPreguntas.innerHTML = "";
+    contenedorPreguntas.classList.add("hidden")
+
+    let resultado = document.getElementById("resultadoEvaluacion");
+    resultado.classList.remove("hidden");
+
+    let id = txtId.value.trim();
+    let personas = JSON.parse(localStorage.getItem("personas")) || [];
+    let persona = personas.find(persona => persona.id === id);
+    resultado.innerHTML = `
+    
+        <div class="resultadoCard">
+
+            <h2>Resultado Final</h2>
+
+            <h3 class="nombreAlumno">
+                ${persona.nombre} ${persona.apellido}
+            </h3>
+
+            <div class="estadisticas">
+
+                <div class="estadistica correctas">
+                    <span>${correctas}</span>
+                    <p>Correctas</p>
+                </div>
+
+                <div class="estadistica incorrectas">
+                    <span>${incorrectas}</span>
+                    <p>Incorrectas</p>
+                </div>
+
+                <div class="estadistica nota">
+                    <span>${nota.toFixed(1)}</span>
+                    <p>Nota Final</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    `;
+
 }
 
 function iniciarEvaluacion(){
@@ -156,6 +250,7 @@ function iniciarEvaluacion(){
     // GUARDAR CAMBIOS
     localStorage.setItem("personas", JSON.stringify(personas));
 
+    btnIniciar.classList.add("hidden")
     renderizarPreguntas();
 }
 
